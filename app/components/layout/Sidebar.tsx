23854a5b7a7aa1ef73d69d65d93c276a9b8a083c
Sultonzoda Abdulloh <sultonzoda2011@gmail.com>
2026-08-05 22:35:48 +0500

    feat: use light/dark logo assets in sidebar

--- app/components/layout/Sidebar.tsx
+++ app/components/layout/Sidebar.tsx
@@ -5,7 +5,10 @@ import { getSidebarConfig, getVisibleNavigation } from '~/config/navigation';
 import { useCan } from '~/hooks/useCan';
 import { NavMain } from './NavMain';
 
-import favicon from '/favicon.ico';
+import darkFavicon from '/dark-favicon.png';
+import lightFavicon from '/light-favicon.png';
+import textInRightDark from '/text-in-right-logo-dark.png';
+import textInRightLight from '/text-in-right-logo-light.png';
 
 export function AppSidebar() {
   const { can } = useCan();
@@ -19,17 +22,16 @@ export function AppSidebar() {
       <SidebarHeader className="px-2">
         <div className="flex items-center justify-start group-data-[collapsible=icon]:justify-center">
           {/* Full logo */}
-          <div className="flex items-center group-data-[collapsible=icon]:hidden">
-            <img src={favicon} className="h-10 w-auto object-contain" alt="Trade CRM" />
-            <span className="font-semibold">TradeCRM</span>
+          <div className="flex items-center p-2 group-data-[collapsible=icon]:hidden">
+            <img src={textInRightLight} className="h-10 w-auto object-contain dark:hidden" alt="Trade CRM" />
+            <img src={textInRightDark} className="hidden h-10 w-auto object-contain dark:block" alt="Trade CRM" />
           </div>
 
           {/* Small logo */}
-          <img
-            src={favicon}
-            className="hidden h-8 w-8 object-contain group-data-[collapsible=icon]:block"
-            alt="Trade CRM"
-          />
+          <div className="hidden items-center justify-center group-data-[collapsible=icon]:flex">
+            <img src={lightFavicon} className="h-8 w-8 object-contain dark:hidden" alt="Trade CRM" />
+            <img src={darkFavicon} className="hidden h-8 w-8 object-contain dark:block" alt="Trade CRM" />
+          </div>
         </div>
       </SidebarHeader>
       <SidebarContent>
