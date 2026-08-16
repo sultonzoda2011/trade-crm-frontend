00f0d11ace42e9916eb484993956d3e56bc3c79d
Sultonzoda Abdulloh <sultonzoda2011@gmail.com>
2026-08-16 05:24:56 +0500

    refactor: use StatRow in market detail page

--- app/routes/(crm)/markets/id/route.tsx
+++ app/routes/(crm)/markets/id/route.tsx
@@ -14,6 +14,7 @@ import { NotFoundBlock } from '~/components/shared/NotFoundBlock';
 import { DetailHeader } from '~/components/shared/DetailHeader';
 import { EntityCard } from '~/components/shared/EntityCard';
 import { InfoItem } from '~/components/shared/InfoItem';
+import { StatRow } from '~/components/shared/StatRow';
 import { InfoLink } from '~/components/shared/InfoLink';
 import { ListLink } from '~/components/shared/ListLink';
 import { MarketEntityTabs } from '~/components/shared/MarketEntityTabs';
@@ -106,7 +107,7 @@ export default function MarketDetailPage() {
             ) : undefined
           }
         />{' '}
-        <div className="border-border mt-3 grid grid-cols-3 gap-2 border-t pt-3">
+        <StatRow className="border-border mt-3 border-t pt-3">
           {' '}
           <StatCard
             size="sm"
@@ -132,7 +133,7 @@ export default function MarketDetailPage() {
             to="/transactions"
             state={filterState}
           />{' '}
-        </div>{' '}
+        </StatRow>{' '}
       </Panel>{' '}
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
         {' '}
