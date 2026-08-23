352a2af3da38f6e4b00246046fae84e451b1d5b7
Sultonzoda Abdulloh <sultonzoda2011@gmail.com>
2026-08-23 18:00:57 +0500

    feat: show email as subInfo in users name column

--- app/routes/(crm)/users/configs/columns.tsx
+++ app/routes/(crm)/users/configs/columns.tsx
@@ -50,7 +50,13 @@ export const getColumns = ({ t }: { t: TFunction }): ColumnDef<User, any>[] => {
     columnHelper.accessor('name', {
       header: t('fields.name'),
       enableHiding: false,
-      cell: (info) => <UserAvatar fullName={info.row.original.name} imagePath={info.row.original.image ?? undefined} />,
+      cell: (info) => (
+        <UserAvatar
+          fullName={info.row.original.name}
+          imagePath={info.row.original.image ?? undefined}
+          subInfo={info.row.original.email}
+        />
+      ),
     }),
     columnHelper.accessor('email', {
       header: t('fields.email'),
